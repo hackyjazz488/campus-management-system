17dc95054cb081a991b55add9b4cab453bd8adb2
hackerjazz448 <mishrariddhi2008@gmail.com>
2025-10-17 09:51:05 +0530

    Update index_user.php

--- index_user.php
+++ index_user.php
@@ -111,11 +111,12 @@ form, .content {
     <!-- logged in user information -->
     <?php  if (isset($_SESSION['username'])) : ?>
     	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
-        <p> <a href="cal.php?logout='1'" style="color: purple;">see event</a> </p>
+        <p> <a href="cal2.php?logout='1'" style="color: purple;">see event</a> </p>
 
     	<p> <a href="webpage.php?logout='1'" style="color: red;">logout</a> </p>
     <?php endif ?>
 </div>
 
 </body>
+
 </html>
